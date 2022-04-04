f006db41b0019e1c14cd59e4cef8af68672a7efb
Utkrisht Singh Chauhan <koq.utx10101@intacht.com>
2022-04-04 23:04:30 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const timestamp = 1646681954250;
+const timestamp = 1649093665279;
 const build = [
-  "/_app/start-8ea2a365.js",
+  "/_app/start-572d52ec.js",
   "/_app/assets/start-464e9d0a.css",
   "/_app/pages/__layout.svelte-b3279b71.js",
   "/_app/error.svelte-2e155d22.js",
@@ -13,10 +13,10 @@ const build = [
   "/_app/pages/_locale_/about/key-persons.svelte-1c0e6d11.js",
   "/_app/pages/_locale_/about/bpf.svelte-3d9bdb3f.js",
   "/_app/pages/_locale_/join/index.svelte-387dcc5b.js",
-  "/_app/pages/_locale_/join/eligibility.svelte-bf541b8a.js",
+  "/_app/pages/_locale_/join/eligibility.svelte-9cb58b9c.js",
   "/_app/pages/_locale_/join/inspector.svelte-5d7d851d.js",
   "/_app/pages/_locale_/join/syllabus.svelte-43c915e0.js",
-  "/_app/pages/_locale_/join/posts.svelte-974e8356.js",
+  "/_app/pages/_locale_/join/posts.svelte-44a68eca.js",
   "/_app/pages/_locale_/join/faq.svelte-a3a8caf7.js",
   "/_app/pages/_locale_/join/si.svelte-af5b3df1.js",
   "/_app/pages/_locale_/_...no_route_.svelte-99147737.js",
